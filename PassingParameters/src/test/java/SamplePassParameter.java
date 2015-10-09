2b8988aa2c0731de6ca665954dc828133334e2d7
idjalalov <ilhomdjalalov@gmail.com>
2015-10-09 05:48:20 -0400

    twenty third

--- PassingParameters/src/test/java/SamplePassParameter.java
+++ PassingParameters/src/test/java/SamplePassParameter.java
@@ -29,6 +29,8 @@ public class SamplePassParameter extends BaseClass{
         if (driver.getPageSource().contains("incorrect")){
             System.out.println("LOGIN TEST WITH INVALID USERNAME AND PASSWORD HAS PASSED SUCCESSFULLY.");
             System.out.println("Auto trigger is on");
+            System.out.println("It's very difficult to configure auto-trigger.");
+
         }
 
     }
